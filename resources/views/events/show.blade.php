bad991b6b692486f7a383b8856134952d16e5645
sanik <alex.radu.1000@gmail.com>
2016-08-04 17:31:55 +0100

    added gmaps & gallery for events

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -3,6 +3,8 @@
 @section('title') <?=$event->title?> :: @parent @endsection
 
 @section('styles')
+<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('lib/bootstrap-image-gallery/css/bootstrap-image-gallery.min.css') }}">
 <style type="text/css">
 .event-header {
   text-align: center;
@@ -24,7 +26,23 @@
   height: 500px;
   margin-top: 30px;
 }
-
+.event-photos {
+  margin-top: 30px;
+}
+.event-photos a {
+  display: inline-block;
+  padding: 0;
+  margin-left: 15px;
+}
+.event-photos a:first-child {
+  margin-left: 0;
+}
+.event-photos a img {
+  width: 100%;
+}
+.event-details {
+  margin-bottom: 15px;
+}
 @media(max-width: 768px) {
   #the-map {
     height: 300px;
@@ -57,8 +75,25 @@
         <p class="event-details">
           <?=$event->time_start?> - <?=$event->time_end?>, <?=$event->price?>, <?=$event->location_name?>
         </p>
+        <p>
+          @if($event->getOriginal('price'))
+          <a target="_blank" class="btn btn-primary" href="<?=$event->tickets_link?>">Tickets&nbsp;<i class="fa fa-ticket" aria-hidden="true"></i></a>
+          @endif
+          <a target="_blank" class="btn btn-primary" href="http://maps.google.com/?q=<?=trim(preg_replace('/\s+/', ' ', $event->location_address))?>">Directions&nbsp;<i class="fa fa-location-arrow" aria-hidden="true"></i></a>
+        </p>
       </div>
     </div>
+    @if(count($event->photos))
+    <div class="row">
+      <div class="col-sm-12 event-photos">
+        @foreach($event->photos as $photo)
+          <a class="col-md-3 col-sm-4 col-xs-6" href="{{ asset('img/events/'.$event->id.'/'.$photo->filename) }}" data-gallery>
+            <img class="img-responsive" src="{{ asset('img/events/'.$event->id.'/'.$photo->filename) }}">
+          </a>
+        @endforeach
+      </div>
+    </div>
+    @endif
   </div>
 </section>
 <section id="map-section">
@@ -69,6 +104,39 @@
       </div>
     </div>
   </div>
+</div>
+<div id="blueimp-gallery" class="blueimp-gallery">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title"></h3>
+    <a class="prev">‹</a>
+    <a class="next">›</a>
+    <a class="close">×</a>
+    <a class="play-pause"></a>
+    <ol class="indicator"></ol>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+    <div class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body next"></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default pull-left prev">
+                        <i class="glyphicon glyphicon-chevron-left"></i>
+                        Previous
+                    </button>
+                    <button type="button" class="btn btn-primary next">
+                        Next
+                        <i class="glyphicon glyphicon-chevron-right"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
 
@@ -103,4 +171,6 @@ function initMap() {
 }
 initMap();
 </script>
+<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+<script src="{{ asset('lib/bootstrap-image-gallery/js/bootstrap-image-gallery.min.js') }}"></script>
 @endsection
